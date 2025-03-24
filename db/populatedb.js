d55e26be08211e3c847fd9d9c8eb924946250ad1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-24 22:18:53 +0800

    Add pool from pg to connect the postgresql to the app. Add a script that popuplate the db direct from app. Add queries file to be use later

---
+++ db/populatedb.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `CREATE TABLE IF NOT EXISTS users(
+user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL,
+isMember BOOLEAN DEFAULT 'false' NOT NULL,
+isAdmin BOOLEAN DEFAULT 'false' NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS messages(
+message_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+message VARCHAR(1024),
+user_id INTEGER NOT NULL,
+CONSTRAINT fk_user
+FOREIGN KEY(user_id)
+REFERENCES users(user_id)
+);
+
+`
+
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+
+  console.log("done");
+
+}
+
+main();
