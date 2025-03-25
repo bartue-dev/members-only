f7effb9c971d3bc8ea7f24fb3a1c488056cd3d42
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-25 22:59:09 +0800

    Set up session and authentication using passportjs passport-local express session and connect-pg-simple as well as bcryptjs for passwrod hashing. Import the passport implementation from authetication/passport.js file

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("node:path");
 
+const passport = require("./authentication/passport");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./db/pool");
+
 const app = express();
+require("dotenv").config()
 
 const indexRouter = require("./routes/indexRouter");
+const signupRouter = require("./routes/signupRouter");
+const homeRouter = require("./routes/homeRouter");
 
 //connect ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//turn req.body into object
-app.use(express.urlencoded({ extended: true}));
-
 //connect static files
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
+//session store user data to session and cookie
+app.use(session({
+  store: new pgSession({
+    pool: pool,
+    tableName: "session"
+  }),
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 1000
+  }
+}));
+
+//invoke passport session for login persistent
+app.use(passport.passportSession);
+//turn req.body into object
+app.use(express.urlencoded({ extended: true}));
+
+passport.passportLocalStrat;
+passport.passportSerializeUser;
+passport.passportDeserializeUser;
+
 //routes
+app.use((req, res, next) => {
+  console.log("session:", req.session);
+  next();
+});
 app.use("/", indexRouter);
+app.use("/sign-up", signupRouter);
+app.post("/log-in", passport.passportAuth)
+app.use("/home", homeRouter);
 
 //error middleware
 app.use((err, req, res, next) => {
