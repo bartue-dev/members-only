2eea629d582ae9b5666326c726d34ef6e933b036
Bartue <[bartbatiancila28@gmail.com]>
2025-03-27 15:42:06 +0800

    Import validateSignup from validator/express-validator file also validationResult from express-validator library.

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,22 +1,32 @@
 const asyncHandler = require("express-async-handler");
+const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const db = require("../db/queries");
+const { validateSignup } = require("../validator/express-validator");
 
 
 exports.getSignUpForm = asyncHandler(async (req, res, next) => {
   res.render("sign-up", {
-    title: "Sign Up"
+    title: "Sign Up",
+    errors: []
   })
 });
 
-exports.postSignUp = asyncHandler(async (req, res, next) => {
-
+exports.postSignUp = [ validateSignup,  asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req);
   const { username } = req.body
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-  await db.signUpUser(username, hashedPassword);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("sign-up", {
+      title: "Sign up",
+      errors: errors.array()
+    });
+  }
 
+  await db.signUpUser(username, hashedPassword);
 
   res.redirect("/");
 
-});
+})
+];
