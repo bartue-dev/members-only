e9a2d428b6ee742a3cbfebeda2aadddb3069b84a
Bartue <[bartbatiancila28@gmail.com]>
2025-03-27 19:36:15 +0800

    Pass the req.session.messages to index/log-in route. Displays the session messages if username and password were incorrect. Added || operator and [] empty array so if the session messages return an empty array instead of undefined

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,10 @@
 const asyncHandler = require("express-async-handler");
 
 exports.getIndex = asyncHandler(async (req, res, next) => {
+  const loginErrMsg = req.session.messages
+
   res.render("index", {
-    title: "Members only"
+    title: "Members only",
+    loginErrMsg: loginErrMsg || []
   });
 });
