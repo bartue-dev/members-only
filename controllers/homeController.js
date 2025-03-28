b6033e9b8a4a49457cd30b6d8b743b93df42ccc9
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-28 10:28:01 +0800

    take all the current users post and render it to home.ejs file. Create a addPost controller that take title, message and user_id and redirect to home after post request

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,25 @@
-const db = require("../db/queries");
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
 
 exports.getHome = asyncHandler(async (req, res, next) => {
+  const user_id = req.user.user_id;
+
+  const myPost = await db.getMyPost(user_id);
+
   res.render("home", {
-    title: "Home"
+    title: "Home",
+    myPost: myPost
   });
+});
+
+exports.addPost = asyncHandler(async (req, res, next) => {
+  const { title, message} = req.body;
+  const user_id = req.user.user_id;
+
+  console.log("user_id?", user_id);
+  
+
+  await db.addPost(title, message, user_id);
+
+  res.redirect("/home");
 });
