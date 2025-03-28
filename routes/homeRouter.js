69556f83721129c352a89f29dd74a003146eeb32
Bartue <[bartbatiancila28@gmail.com]>
2025-03-28 10:32:33 +0800

    Import isAuth middleware and pass it to homeRouter.(isAuth) so all the homeRouter will be authenticate before access

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
 const homeController = require("../controllers/homeController");
 const homeRouter = Router();
+const { isAuth } = require("../authentication/authMiddleware")
+
+homeRouter.use(isAuth);
 
 homeRouter.get("/", homeController.getHome);
+
+homeRouter.post("/add-post", homeController.addPost);
 
 module.exports = homeRouter;
