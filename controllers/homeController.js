cd911e04e880e0689129e2e1439d30dce8fae5c6
Bartue <[bartbatiancila28@gmail.com]>
2025-03-28 23:35:38 +0800

    Add validation for addPost controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
+const { validationResult } = require("express-validator");
+const { validatePost } = require("../validator/express-validator");
 
 exports.getHome = asyncHandler(async (req, res, next) => {
   const user_id = req.user.user_id;
@@ -8,18 +10,27 @@ exports.getHome = asyncHandler(async (req, res, next) => {
 
   res.render("home", {
     title: "Home",
-    myPost: myPost
+    myPost: myPost,
+    errors: []
   });
 });
 
-exports.addPost = asyncHandler(async (req, res, next) => {
+exports.addPost = [validatePost, asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req);
   const { title, message} = req.body;
   const user_id = req.user.user_id;
+  const myPost = await db.getMyPost(user_id);
 
-  console.log("user_id?", user_id);
-  
+  if (!errors.isEmpty()) {
+    return res.status(400).render("home", {
+      title: "Home",
+      myPost: myPost,
+      errors: errors.array()
+    })
+  }
 
   await db.addPost(title, message, user_id);
 
   res.redirect("/home");
-});
+})
+];
