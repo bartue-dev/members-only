fe5c53870781c7743530033b7ea48d7fd0c2a6e2
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-28 10:30:04 +0800

    Create a addPost query that insert the title,message and user_id to post table. Also create a getMyPost query that get all the current user post

--- db/queries.js
+++ db/queries.js
@@ -4,6 +4,18 @@ async function signUpUser(username, password) {
   await pool.query("INSERT INTO users(username, password) VALUES ($1, $2)", [username, password])
 }
 
+async function addPost(title, message, user_id) {
+  await pool.query("INSERT INTO post(title, message, user_id) VALUES($1, $2, $3)", [title, message, user_id]);
+}
+
+async function getMyPost(user_id) {
+  const { rows } = await pool.query("SELECT users.username, post.title, post.message FROM post INNER JOIN users ON users.user_id = post.user_id WHERE post.user_id = $1", [user_id]);
+
+  return rows;
+}
+
 module.exports = {
-  signUpUser
+  signUpUser,
+  addPost,
+  getMyPost
 }
