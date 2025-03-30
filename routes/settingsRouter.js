2d08a2f1635918f69726effd18ed66e338e727ed
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-30 13:08:41 +0800

    Import postMembership controller to post request

--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -3,8 +3,11 @@ const settingsRouter = Router();
 const settingsController = require("../controllers/settingsController");
 const { isAuth } = require("../authentication/authMiddleware")
 
+//authenticate user before access the routes
 settingsRouter.use(isAuth);
 
 settingsRouter.get("/", settingsController.getSettings);
+
+settingsRouter.post("/membership", settingsController.postMembership);
 
 module.exports = settingsRouter;
