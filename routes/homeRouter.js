7f4d62b9b4ee3886f210a5d4945b55ae22eddd7a
Bartue <[bartbatiancila28@gmail.com]>
2025-03-30 13:07:08 +0800

    Add comment

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -3,10 +3,13 @@ const homeController = require("../controllers/homeController");
 const homeRouter = Router();
 const { isAuth } = require("../authentication/authMiddleware")
 
+//authenticate user before access the routes
 homeRouter.use(isAuth);
 
 homeRouter.get("/", homeController.getHome);
 
 homeRouter.post("/add-post", homeController.addPost);
+
+homeRouter.post("/:user_id/delete", homeController.postDeleteUser);
 
 module.exports = homeRouter;
