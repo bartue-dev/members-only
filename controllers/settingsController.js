e4f260b321a05526de3e08d0890b65595854c054
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-30 12:20:41 +0800

    Import db/queries.js then created a postMembership that allow the user to be a member after taking a quiz in settings page. No criteria need

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -1,8 +1,16 @@
 const asyncHandler = require("express-async-handler");
-
+const db = require("../db/queries");
 
 exports.getSettings = asyncHandler(async (req, res, next) => {
   res.render("settings", {
     title: "Settings"
   });
+});
+
+exports.postMembership = asyncHandler(async (req, res, next) => {
+  const user_id = req.user.user_id;
+
+  await db.toggleMembership(user_id);
+
+  res.redirect("/settings");
 });
