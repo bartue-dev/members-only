f43bb3bf865d27d6b77377f1e456166504911dfb
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-30 12:18:25 +0800

    Create a if else statement. If user is not log in render index/login page otherwise redirect user to home route

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,8 +3,13 @@ const asyncHandler = require("express-async-handler");
 exports.getIndex = asyncHandler(async (req, res, next) => {
   const loginErrMsg = req.session.messages
 
-  res.render("index", {
-    title: "Members only",
-    loginErrMsg: loginErrMsg || []
-  });
+  //if user is not log in render index/log-in. If user is log in redirect to home route
+  if (!req.user) {
+    res.render("index", {
+      title: "Members only",
+      loginErrMsg: loginErrMsg || []
+    });
+  } else {
+    res.redirect("/home");
+  }
 });
