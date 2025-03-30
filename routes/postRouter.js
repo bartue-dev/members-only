86ade416e48904ca29289af494dab2e03c9fdc29
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-30 13:08:05 +0800

    import postDeletePost middleware to a post request

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,8 +3,11 @@ const postRouter = Router();
 const postController = require("../controllers/postController");
 const { isAuth } = require("../authentication/authMiddleware");
 
+//authenticate user before access the routes
 postRouter.use(isAuth);
 
 postRouter.get("/", postController.getPost);
+
+postRouter.post("/:post_id/delete", postController.postDeletePost);
 
 module.exports = postRouter;
