ffb0926bd0b6ce153dad55e4ee8497d524bcbd5b
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-31 21:21:21 +0800

    Remove the ALTER TABLE QUERY then make the ssid PRIMARY directly to the session TABLE. Then add a IF NOT EXIST clause to CREATE INDEX

--- db/populatedb.js
+++ db/populatedb.js
@@ -24,15 +24,13 @@ ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "session" (
-  "sid" varchar NOT NULL COLLATE "default",
+  "sid" varchar NOT NULL COLLATE "default" PRIMARY KEY,
   "sess" json NOT NULL,
   "expire" timestamp(6) NOT NULL
 )
 WITH (OIDS=FALSE);
 
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
-CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
 
 `
 
