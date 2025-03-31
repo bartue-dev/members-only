baf8bd84f9b0945d30cdd93979ac297cc9fdeccb
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-31 21:20:10 +0800

    Add a postAdmin controller middleware to become an admin

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -3,7 +3,8 @@ const db = require("../db/queries");
 
 exports.getSettings = asyncHandler(async (req, res, next) => {
   res.render("settings", {
-    title: "Settings"
+    title: "Settings",
+    incorrectAnsMsg: false
   });
 });
 
@@ -13,4 +14,19 @@ exports.postMembership = asyncHandler(async (req, res, next) => {
   await db.toggleMembership(user_id);
 
   res.redirect("/settings");
+});
+
+exports.postAdmin = asyncHandler(async (req, res, next) => {
+  const user_id = req.user.user_id;
+  const { linux }= req.body
+ 
+  if (linux.toLowerCase() !== "linus torvalds") {
+    res.render("settings", {
+      title: "Settings",
+      incorrectAnsMsg: true
+    });
+  } else {
+    await db.toggleisAdmin(user_id);
+    res.redirect("/settings");
+  }
 });
