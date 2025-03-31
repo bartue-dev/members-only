5b5f92075e15c3b90f2e8af3cd782117f940327d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-31 15:05:58 +0800

    replace to connectionString the cliend connecting to db

--- db/populatedb.js
+++ db/populatedb.js
@@ -29,11 +29,7 @@ ON DELETE CASCADE
 async function main() {
   console.log("seeding...");
   const client = new Client({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
+    connectionString: process.env.DATABASE_URL
   });
 
   await client.connect();
